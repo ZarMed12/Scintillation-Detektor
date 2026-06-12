9f4ce897d82b3fe2e6a0327d37235b6bec72dfdb
ZarMed12 <mzprivatni@gmail.com>
2026-06-12 13:52:18 +0200

    Update SDCard.py

--- Raspberry Pi Pico W/SDCard.py
+++ Raspberry Pi Pico W/SDCard.py
@@ -9,7 +9,7 @@ def log_event(count):
         t[0], t[1], t[2], t[4], t[5], t[6]
     )
 
-    line = "{} | {x -type of detection radiation}: {}\n".format(timestamp, count)
+    line = "{} | {x - type of detected radiation}: {}\n".format(timestamp, count)
 
     # Save to SD card
     with open("/sd/x_log.txt", "a") as f:
